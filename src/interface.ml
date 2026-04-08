2a5d6ae81cb097fbf844b932e10436c4d55c9025
Humam Alhusaini <humamalhusaini@tutamail.com>
2026-04-08 18:44:57 -0500

    made a goal printer

--- src/interface.ml
+++ src/interface.ml
@@ -1,18 +1,16 @@
-(* the Ltac2 plugin is "packaged" ie its modules are all contained in module Ltac2_plugin
+open Ltac2_plugin  (* the Ltac2 plugin is "packaged" ie its modules are all contained in module Ltac2_plugin
    without this open we would have to refer to eg Ltac2_plugin.Tac2externals below *)
-open Ltac2_plugin
-
-(* APIs to register new externals, including the convenience "@->" infix operator *)
-open Tac2externals
-
-(* Translation operators between Ltac2 values and OCaml values in various types *)
-open Tac2ffi
+open Tac2externals  (* APIs to register new externals, including the convenience "@->" infix operator *)
+open Tac2ffi  (* Translation operators between Ltac2 values and OCaml values in various types *)
+(*open Proofview *) (*Managing the proof context*)
+(*open Proof*) (*Similar to Proofview*)
+(*open Printer*) (*Pretty printing*)
 
 (* Used to distinguish our primitives from some other plugin's primitives.
    By convention matches the plugin's ocamlfind name. *)
 let plugin_name = "rocq_z3.rocq_z3"
 
-let pname s = { Tac2expr.mltac_plugin = plugin_name; mltac_tactic = s }
+let pname s : Tac2expr.ml_tactic_name = { Tac2expr.mltac_plugin : string = plugin_name; mltac_tactic : string = s }
 
 (* We define for convenience a wrapper around Tac2externals.define.
    [define "foo"] has type
@@ -23,8 +21,29 @@ let pname s = { Tac2expr.mltac_plugin = plugin_name; mltac_tactic = s }
    specification. *)
 let define s = define (pname s)
 
-let () = define "smt" (string @-> ret unit) @@ fun name ->
-  let chan = name ^ ".smt2" |> open_out  in 
+(* Rocq tactics are values of the [Proofview.tactic] monad.
+  tclUnit in Proofview is the return operation of this monad.
+   We define an alias for convenience. *)
+let return = Proofview.tclUNIT
+
+let smt : (string -> unit) = fun name ->
+  let chan = name ^ ".smt2" |> open_out in 
     let _ = Printf.fprintf chan "67" in
       flush chan;; 
+
+let get_goal_string () : string Proofview.tactic =
+  Proofview.Goal.enter_one (fun gl ->
+    let my_econstr = Proofview.Goal.concl gl in
+    let env = Proofview.Goal.env gl in
+    let sigma = Tacmach.project gl in
+    let s = Pp.string_of_ppcmds (Printer.pr_econstr_env env sigma my_econstr) in
+    return s
+  );;
+
+let rand_string () = "hello";;
+
+let () = 
+  define "smt" (string @-> ret unit) @@ smt;
+  define "print_goal" (unit @-> tac string) @@ get_goal_string;
+  define "rand_string" (unit @-> ret string) @@ rand_string;;
 
--- theories/Demo.v
+++ theories/Demo.v
@@ -3,7 +3,17 @@ From Rocq_Z3 Require Import Loader.
 
 Ltac2 Eval smt "hello".
 
+Ltac2 Eval rand_string ().
+
 Goal 1 + 1 = 2.
 Proof.
+  Ltac2 Eval print_goal ().
   smt "z3".
+Admitted.
+
+Require Import Nat.
+
+Theorem what : exists x, x = 1.
+Proof.
+  Ltac2 Eval print_goal ().
 Admitted.
