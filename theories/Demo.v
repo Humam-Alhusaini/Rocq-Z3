6b36ceaa1a04ccda863b5a3ec828c670d150c4b5
Humam Alhusaini <humamalhusaini@tutamail.com>
2026-04-09 15:54:43 -0500

    added tactic for printing so that I can return a string rather than making a file. Making a file comes later when I translate to SMT2. Also added 2 hyp theorems

--- theories/Demo.v
+++ theories/Demo.v
@@ -1,6 +1,9 @@
 From Ltac2 Require Import Ltac2.
 From Rocq_Z3 Require Import Loader.
 
+Ltac2 print (s : string) : unit :=
+  Message.print (Message.of_string s).
+
 Goal 1 + 1 = 2.
 Proof.
   print_goal "goal".
@@ -26,11 +29,22 @@ Admitted.
 
 Theorem forall_nintro : forall x, x = 1.
 Proof.
-print_goal "nintro".
+  print_goal "nintro".
 Admitted.
 
 Theorem forall_intro : forall x, x = 1.
 Proof.
   intro x.
-print_goal "intro".
+  print_goal "intro".
+Admitted.
+
+Theorem hyp_intro : forall x, x = 1 -> 1 = x.
+Proof.
+  intros x H.
+  print_goal "hyp_intro".
+Admitted.
+
+Theorem hyp_nintro : forall x, x = 1 -> 1 = x.
+Proof.
+  print_goal "hyp_nintro".
 Admitted.
--- theories/Loader.v
+++ theories/Loader.v
@@ -5,6 +5,3 @@ Declare ML Module "rocq_z3.rocq_z3".
 (** a wrapper around "print_goal" *)
 Ltac2 @external print_goal : string -> unit
   := "rocq_z3.rocq_z3" "print_goal".
-
-
-
