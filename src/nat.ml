7f7819723ea459ff004ecd9e1773e56493d5b68a
Humam Alhusaini <humamalhusaini@tutamail.com>
2026-04-12 16:56:45 -0500

    fixed id and path

--- src/nat.ml
+++ src/nat.ml
@@ -3,52 +3,32 @@ open Paths
 
 open Z3
 
-let num_to_expr (i : int) (ctx : context) (mk_num : context -> int -> Expr.expr) : Expr.expr = 
-  mk_num ctx i;;
-
 let app_to_list (constr : Constr.t) : Constr.t list = 
   match Constr.kind constr with
   | App (constr', constrs) -> constr' :: (Array.to_list constrs)
   | _ -> failwith "app_to_list: not an App";;
 
-module NatNums = struct
+let get_name_from_ind ((mutind, _) : Ind.t) : Id.t = 
+  MutInd.label mutind;;
+ 
+let eq_path (ind : Ind.t) (path : ModPath.t) (id : Id.t) : bool =
+  (Ind.modpath ind |> ModPath.equal path && get_name_from_ind ind |> Id.equal id)
 
-  let num_to_SO (n : int) : int = 
-    match n with
-    | 1 -> 0
-    | 2 -> 1
-    | _ -> failwith (Printf.sprintf "num_to_SO: unexpected constructor index %d" n);;
+let rec lconstr_to_nat (lconstr : Constr.t list) (nat : int) : int = 
+  match lconstr with
+  | [] -> failwith "oh so empty"
+  | constr :: _ -> constr_to_nat constr nat
 
-  let constructor_to_SO ((ind, num) : constructor) =
-    match Ind.modpath ind |> ModPath.equal nat_modpath with
-    | false -> failwith (Printf.sprintf "construct_to_SO: not a nat constructor, modpath was %s" (ModPath.to_string (Ind.modpath ind)))
-    | true -> num_to_SO num;;
-
-  let constr_to_SO (constr : Constr.t) : int = 
-    match Constr.kind constr with
-    | Construct (constructor, _) -> constructor_to_SO constructor
-    | _ -> failwith "constr_to_SO: not a Construct";;
-
-  let rec lconstr_to_nat (lconstr : Constr.t list) (nat : int) : int = 
-    match lconstr with
-    | hd :: ls -> lconstr_to_nat ls (constr_to_SO hd + nat)
-    | [] -> nat;;
-
-  let rec constr_to_nat (lconstr : Constr.t list) (nat : int) : int = 
-    match lconstr with
-    | [] -> failwith "oh so empty"
-    | constr :: _ ->
-    (match Constr.kind constr with
-    | App (constr', constrs) -> 1 + constr_to_nat (Array.to_list constrs) nat
-    | Construct _ -> 0
-    | _ -> failwith "constr_to_nat: not an App or Construct");;
-
-end
+and constr_to_nat (constr : Constr.t) (nat : int) : int = 
+  match Constr.kind constr with
+  | App (constr', constrs) -> 1 + lconstr_to_nat (Array.to_list constrs) nat
+  | Construct _ -> 0
+  | _ -> failwith "constr_to_nat: not an App or Construct";;
 
 module Typ = struct
 
   let ind_to_typ (ind : inductive) =
-    match Ind.modpath ind |> ModPath.equal nat_modpath with
+    match eq_path ind nat_modpath natid with
     | false -> failwith (Printf.sprintf "ind_to_typ: not a nat ind, modpath was %s" (ModPath.to_string (Ind.modpath ind)))
     | true -> Arithmetic.Integer.mk_numeral_i;;
 
@@ -62,7 +42,7 @@ end
 module Logic = struct
 
   let ind_to_eq (ind : inductive) =
-    match Ind.modpath ind |> ModPath.equal eq_modpath with
+    match eq_path ind eq_modpath eqid with
     | false -> failwith (Printf.sprintf "ind_to_eq: not an eq ind, modpath was %s" (ModPath.to_string (Ind.modpath ind)))
     | true -> Boolean.mk_eq;;
 
@@ -78,7 +58,7 @@ let parse_entire (constr : Constr.t) (ctx : context) : Expr.expr =
   match constrs with
   | eq :: typ :: num1' :: num2' :: _ -> 
       let ftyp = Typ.constr_to_typ typ ctx in
-      let num1 = NatNums.constr_to_nat [num1'] 0 |> ftyp in
-      let num2 = NatNums.constr_to_nat [num2'] 0 |> ftyp in
+      let num1 = lconstr_to_nat [num1'] 0 |> ftyp in
+      let num2 = lconstr_to_nat [num2'] 0 |> ftyp in
       (Logic.constr_to_log eq ctx) num1 num2
   | _ -> failwith "your mom";;
--- src/paths.ml
+++ src/paths.ml
@@ -1,14 +1,12 @@
 open Names
 
-
-(*TODO Have a whitelist of supported modules, others get rejected*)
-
 let corelib = "Corelib"
 let stdlib = "Stdlib"
 
 let corelib_init = [corelib; "Init"]
 let corelib_numbers = [corelib; "Numbers"]
 let stdlib_narith = [stdlib; "NArith"]
+
 
 let make_modpath parts =
   MPfile (DirPath.make (List.map Id.of_string (List.rev parts)))
@@ -18,3 +16,6 @@ let eq_modpath        = make_modpath (corelib_init @ ["Logic"])
 let binnat_modpath    = make_modpath (stdlib_narith @ ["BinNat"; "N"])
 let binnatdef_modpath = make_modpath (stdlib_narith @ ["BinNatDef"; "N"])
 let binnums_modpath   = make_modpath (corelib_numbers @ ["BinNums"; "N"])
+
+let natid = Id.of_string "nat";;
+let eqid = Id.of_string "eq";;
