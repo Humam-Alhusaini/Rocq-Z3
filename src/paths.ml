fc51c9284f81b975f977b890487d603b5daeee3b
Humam Alhusaini <humamalhusaini@tutamail.com>
2026-04-15 19:03:53 -0500

    cleaned up paths

--- src/paths.ml
+++ src/paths.ml
@@ -1,6 +1,3 @@
-open Names
-open Z3
-
 type coqTerm = Constr.t lazy_t
 
 let gc (prefix : string) (constant : string) : Constr.t lazy_t =
@@ -60,30 +57,4 @@ let eq_prefix = "core.eq"
 let eq_gc = gc eq_prefix
 let ceq = eq_gc "type"
 let crefl_equal = eq_gc "refl"
-
-let make_modpath parts =
-  MPfile (DirPath.make (List.map Id.of_string (List.rev parts)))
-
-let datatype_path       = make_modpath ["Corelib"; "Init"; "Datatypes"]
-let logic_path        = make_modpath ["Corelib"; "Init"; "Logic"]
-
-let natid = Id.of_string "nat";;
-let eqid = Id.of_string "eq";;
-
-let ind_id ((mutind, _) : Ind.t) : Id.t = 
-  MutInd.label mutind;;
- 
-let eq_path (ind : Ind.t) (path : ModPath.t) (id : Id.t) : bool =
-  (Ind.modpath ind |> ModPath.equal path && ind_id ind |> Id.equal id)
-
-let path_error (ind : Ind.t) (id : Id.t) = 
-  let modpathstr = Ind.modpath ind |> ModPath.to_string in
-  let idstr = ind_id ind |> Id.to_string in
-  Printf.sprintf "Not an %s ind, modpath was %s and id was %s" (Id.to_string id)  modpathstr idstr |> failwith;;
-
-
-let known_constants = [
-  ((datatype_path, natid),  Arithmetic.mk_add);
-  ((logic_path, eqid),  Arithmetic.mk_mul);
-]
 
